c9e7f4ce2da6fb36259b593a14e2c43dc1d098c7
Haxorouse <45926268+Haxorouse@users.noreply.github.com>
2022-09-07 18:04:27 -0400

    p1a complete

--- project1a/src/phonebook.rb
+++ project1a/src/phonebook.rb
@@ -1,20 +1,69 @@
 class PhoneBook
     def initialize
+        @names = Array.new(0)
+        @numbers = Array.new(0)
+        @listed = Array.new(0)
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        #puts "add"
+        if lookupByNum(number)
+            if is_listed
+                return false
+            end
+        end
+        for i in @names
+            if i == name
+                return false
+            end
+        end
+        unless  number =~ /\d\d\d-\d\d\d-\d\d\d\d/
+            return false
+        end
+        #conditions
+        @names.push(name)
+        @numbers.push(number)
+        @listed.push(is_listed)
+        return true
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        #puts "name"
+        i = 0
+        while i < @names.length
+            if @names[i] == name && @listed[i]
+                return @numbers[i]
+                break
+            end
+            i += 1
+        end
+        return nil
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        #puts "num"
+        i = 0
+        while i < @numbers.length
+            if @numbers[i] == number && @listed[i]
+                return @names[i]
+                break
+            end
+            i += 1
+        end
+        return nil
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        #puts "ac"
+        out = Array.new(0)
+        i = 0
+        while i < @numbers.length
+            #puts "reading #{@numbers[i][0..2]}"
+            if @numbers[i][0..2] == areacode
+                out.push(@names[i])
+            end
+            i += 1
+        end
+        return out
     end
 end
